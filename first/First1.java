144349f0f64ca14410e2a53c99d4f3fbd53aadf3
engtabark2023 <151608841+engtabark2023@users.noreply.github.com>
2024-01-22 14:11:40 +0300

    Add files via upload

---
+++ first/First1.java
@@ -0,0 +1,78 @@
+package first;
+
+public class First1 
+{
+	public static void main (String args[])
+	{
+		/*
+		String name ="ali"; // variable type
+		int age = 10;
+		System.out.println("hello");
+		System.out.println("my name is "+name+" and my age "+age);
+		
+		
+		
+		int x=47;// if statement
+		if(x%2 == 0)
+		{
+			System.out.println("number is even");
+		}
+		else 
+		{
+			System.out.println("number is ood");
+		}
+		
+		
+		int y=2 ; //switch statement
+		int num1=7,num2=8;
+		switch(y)
+		{
+		case 1:
+			System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+		        break;
+		case 2:
+			 System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+		        break;
+		case 3: 
+			System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+		        break;
+		case 4: 
+			System.out.println(num1 + " / " + num2 + " = " + ((float)num1 /(float)num2));
+		        break;
+		case 5:  
+			System.out.println(num1 + " mod " + num2 + " = " + (num1 % num2));
+		        break;
+		default :
+			System.out.println("inter number btween 1-5");
+		}
+		
+		int sum = 0; // for loop
+	    int n = 1000;
+	    for (int i = n; i >= 1; --i)
+	    {
+	      sum += i;     	 
+	      }
+	    System.out.println("Sum = " + sum);
+	  }*/
+	
+	int mark=1 ,totalsum =0; // while loop
+	float av ;
+	int deg1=40,deg2=90,deg3=80;
+	int c = 1;
+	while(c <=3)
+	{
+		totalsum = deg1+deg2+deg3;
+		c++;
+	}
+    System.out.println("Sum = " + totalsum);
+    av = totalsum/3;
+    System.out.println("average = " + av);
+
+
+	}
+	}
+	
+	
+	
+
+
